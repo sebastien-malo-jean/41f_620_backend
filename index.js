030b32da0ca33bfb71d87581a176bbe9869df9b7
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-02-04 16:01:04 -0500

    Refactoriser le serveur pour utiliser Express et implémenter des routes RESTful pour les films ; ajouter des fichiers HTML et CSS pour le frontend

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-const http = require("http");
+// Importation des librairies
+const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Accueil");
-  } else if (req.url === "/patate") {
-    res.end("Patate");
-  } else {
-    res.end("404");
-  }
+const path = require("path");
+const { log } = require("console");
+
+const server = express();
+dotenv.config();
+
+// Permission d'acces au dossier
+const dossierPublic = path.join(__dirname, "public");
+server.use(express.static(dossierPublic));
+
+//fonctions middleWare
+
+function authentification(req, res, next) {
+  console.log("Authentification en cours...");
+  next();
+}
+/*
+ * Routes servant à récuperer tous les folms de la base de données.
+ */
+// get
+server.get("/", (req, res) => {
+  const reponse = {
+    msg: "Tiguidou",
+  };
+  return res.json(reponse);
+});
+
+server.get("/films", authentification, (req, res) => {
+  return res.json({ msg: "ok films" });
+});
+server.get("/films/:id", (req, res) => {
+  return res.json({ msg: "ok film id" });
+});
+// post
+server.post("/films", (req, res) => {
+  return res.json({ msg: "ok film post" });
+});
+server.post("/films/initialisation", (req, res) => {
+  return res.json({ msg: "ok film initialisation" });
+});
+// put
+server.put("/films/:id", (req, res) => {
+  return res.json({ msg: "ok film" });
+});
+// delete
+server.delete("/films/:id", (req, res) => {
+  return res.json({ msg: "ok film id" });
+});
+// ressource 404
+server.use((req, res) => {
+  res.statusCode = 404;
+  return res.json({ msg: "Ressource non trouvée." });
 });
 
 server.listen(process.env.PORT, () => {
-  console.log("Le serveur est en écoute sur le port ${process.env.PORT}");
+  console.log(`Le serveur est en écoute sur le port ${process.env.PORT}`);
 });
